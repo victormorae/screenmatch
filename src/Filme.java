5695dd64b21febf8a5e03320026fddfed16de775
Victor Moraes <victor.moraes18.8.75@gmail.com>
2025-12-14 21:39:54 -0300

    initial commit

--- src/Filme.java
+++ src/Filme.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme:" + nome);
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+
+    }
+
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
+
+    }
 }
